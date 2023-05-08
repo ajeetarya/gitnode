f82edd9235a9af0f467c5c342243133b264b3256
ajeetarya <65396424+ajeetarya@users.noreply.github.com>
2023-05-08 16:06:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello SRE Team");
+    response.end("Hello SRE Cohart 20 Team");
 });
 
 const port = process.env.PORT || 1337;
